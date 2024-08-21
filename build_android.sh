126d0daf6f118a5ab5dda87c43bc2aac901fb5f7
Zhou Zhenglong <zlzhou.sh@outlook.com>
2024-08-21 17:21:51 +0800

    simplify build_android.sh

--- build_android.sh
+++ build_android.sh
@@ -1,8 +1,5 @@
-[ -z "$GITHUB_WORKSPACE" ] && GITHUB_WORKSPACE="$( cd "$( dirname "$0" )"/.. && pwd )"
 VERSION=$1
-ARCH="$2"
-
-cd $GITHUB_WORKSPACE
+ARCH=$2
 
 git clone https://github.com/nodejs/node.git --branch=v$VERSION --depth=1
 
@@ -10,5 +7,5 @@ cd node
 git apply ../patches/v$VERSION.android.patch
 
 # NDK r27, SDK r28
-./android-configure ~/android-ndk-r27 28 $2
+./android-configure ~/android-ndk-r27 28 $ARCH
 make -j4
