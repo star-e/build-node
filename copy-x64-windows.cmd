207c5e8dd5e61b892225645b48b9cae9e2bc08e0
Zhou Zhenglong <zlzhou.sh@outlook.com>
2024-08-22 18:17:51 +0800

    disable pdb

--- copy-x64-windows.cmd
+++ copy-x64-windows.cmd
@@ -20,10 +20,11 @@ md installed\nodejs\x64-windows\debug\bin
 md installed\nodejs\x64-windows\debug\lib
 
 copy node\out\Release\libnode.dll .\installed\nodejs\x64-windows\bin\
-copy node\out\Release\libnode.exp .\installed\nodejs\x64-windows\lib\
 copy node\out\Release\libnode.lib .\installed\nodejs\x64-windows\lib\
-copy node\out\Release\libnode.pdb .\installed\nodejs\x64-windows\lib\
+@REM copy node\out\Release\libnode.exp .\installed\nodejs\x64-windows\lib\
+@REM copy node\out\Release\libnode.pdb .\installed\nodejs\x64-windows\lib\
 copy node\out\Debug\libnode.dll .\installed\nodejs\x64-windows\debug\bin\
-copy node\out\Debug\libnode.exp .\installed\nodejs\x64-windows\debug\lib\
 copy node\out\Debug\libnode.lib .\installed\nodejs\x64-windows\debug\lib\
-copy node\out\Debug\libnode.pdb .\installed\nodejs\x64-windows\debug\lib\
+@REM copy node\out\Debug\libnode.exp .\installed\nodejs\x64-windows\debug\lib\
+@REM copy node\out\Debug\libnode.pdb .\installed\nodejs\x64-windows\debug\lib\
+
